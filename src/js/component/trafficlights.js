c2add6707bb3bf31c36f47c6149dda563f5764d8
SilMontes <Silmontes2001@gmail.com>
2021-03-08 03:02:07 +0000

    Add useState

--- src/js/component/trafficlights.js
+++ src/js/component/trafficlights.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 
 //create your first component
 export function TrafficLights() {
+	const [spotlight, setSpotlight] = useState("");
+
 	return (
 		<div className="container">
-			<div className="circle red" color="red"></div>
-			<div className="circle yellow" color="yellow"></div>
-			<div className="circle green" color="green"></div>
+			<div
+				onClick={() => setSpotlight("red")}
+				className={spotlight === "red" ? "circle-red" : ""}></div>
+			<div
+				onClick={() => setSpotlight("yellow")}
+				className={spotlight === "yellow" ? "circle-yellow" : ""}></div>
+			<div
+				onClick={() => setSpotlight("green")}
+				className={spotlight === "green" ? "circle-green" : ""}></div>
 		</div>
 	);
 }
