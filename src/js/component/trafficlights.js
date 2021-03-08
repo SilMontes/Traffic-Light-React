ae1e477c6f56de8e240faf109bc532e02e34532e
SilMontes <Silmontes2001@gmail.com>
2021-03-08 23:11:13 +0000

    adding interval

--- src/js/component/trafficlights.js
+++ src/js/component/trafficlights.js
@@ -1,10 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 
 //create your first component
 export function TrafficLights() {
-	const [spotlight, setSpotlight] = useState("");
+	const [spotlight, setSpotlight] = useState("green");
+	useEffect(() => {
+		const changing = setInterval(() => {
+			spotlight === "green"
+				? setSpotlight("red")
+				: spotlight === "yellow"
+				? setSpotlight("green")
+				: spotlight === "red"
+				? setSpotlight("yellow")
+				: null;
+		}, 5000);
+		return () => clearInterval(changing);
+	}, [spotlight]);
 
 	return (
 		<div className="container">
